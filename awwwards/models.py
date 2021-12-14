253f08fc66e0cc8deffc1a656d0e06edeb2f2663
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:54:01 +0300

    created Class Rating integer fields 2.0

--- awwwards/models.py
+++ awwwards/models.py
@@ -58,8 +58,7 @@ class Post(models.Model):
         self.save()
 
 class Rating(models.Model):
-    
-    
+    rating = ((1, '1'),(2, '2'),(3, '3'),(4, '4'),(5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10'),)
     design = models.IntegerField(choices=rating, default=0, blank=True)
     usability = models.IntegerField(choices=rating, blank=True)
     creativity = models.IntegerField(choices=rating, blank=True)
