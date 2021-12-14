e4cef56801b4ac2c047c7d131c42bbbe9cb59423
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 09:21:05 +0300

    created test for post

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -16,3 +16,32 @@ class TestProfile(TestCase):
     def test_delete_user(self):
         self.user.delete()
 
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='Vickie')
+        self.post = Post.objects.create(id=1, title='test post', photo='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', description='desc',
+                                        user=self.user, url='http://ur.coml')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Post.all_posts()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) < 1)
+
