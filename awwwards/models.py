ec65ec1a3d4d0364d2476b45dd5c2e7400c79611
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:44:47 +0300

    imported ImageField from pyuploadcare

--- awwwards/models.py
+++ awwwards/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
