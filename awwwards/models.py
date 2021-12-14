c6f33ffbe996e765b6502b34368fa1e8f1d742c0
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:45:50 +0300

    imported from django_countries.fields CountryField

--- awwwards/models.py
+++ awwwards/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from pyuploadcare.dj.models import ImageField
+from django_countries.fields import CountryField
 
 # Create your models here.
