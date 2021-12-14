282c1e81dc68e9f80027236e690a513865de34f9
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:27:40 +0300

    Added Installed apps

--- vintasia/settings.py
+++ vintasia/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'awwwards',
+    'crispy_forms',
+    'rest_framework',
+    'pyuploadcare.dj',
+    'star_ratings',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
