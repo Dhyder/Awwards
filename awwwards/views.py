c1d882c9159b1598365e0b38201c6419ccc02113
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:58:04 +0300

    imported User HttpResponseRedirect

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, Rating
-
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 # Create your views here.
