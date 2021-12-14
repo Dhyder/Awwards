aea6eabc449be97f31613c56d311dd24211908b8
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:59:29 +0300

    imported restframework and forms

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, Rating
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .serializers import ProfileSerializer, UserSerializer, PostSerializer
+from django.contrib.auth import login, authenticate
+from rest_framework import viewsets
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm
+import random
+
 # Create your views here.
