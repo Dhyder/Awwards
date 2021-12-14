8aece4a56dc97e9a46b4bae4535bec32cb27ed3e
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 09:23:25 +0300

    created test for RatingTest

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -45,3 +45,24 @@ class PostTest(TestCase):
         post = Post.search_project('test')
         self.assertTrue(len(post) < 1)
 
+
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='Vickie')
+        self.post = Post.objects.create(id=1, title='test post', photo='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', description='desc',
+   user=self.user, url='http://ur.coml')
+        self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, post=self.post)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = Rating.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+    def test_get_post_rating(self, id):
+        self.rating.save()
+        rating = Rating.get_ratings(post_id=id)
+        self.assertTrue(len(rating) == 1)
+
