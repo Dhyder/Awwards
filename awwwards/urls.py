b47f0a01fab635437f49bbf90455ea2c1a807d21
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 09:09:08 +0300

    defined routers and registered

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -4,7 +4,10 @@ from . import views
 from rest_framework import routers
 
 
-
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
+router.register('profile', views.ProfileViewSet)
 
 
 urlpatterns = [
