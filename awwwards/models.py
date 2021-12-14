8073623c74810452bb741a79da747272d947e3bb
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:48:29 +0300

    created class Profile

--- awwwards/models.py
+++ awwwards/models.py
@@ -8,3 +8,10 @@ from star_ratings.models import Rating
 import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/', default='jigsaw.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = CountryField(blank_label='(select country)', default='KE')
+    contact = models.EmailField(max_length=100, blank=True)
