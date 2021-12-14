ad4c3c0aab20349a64dd91248f9cf24ecf6a911a
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 08:54:59 +0300

    created classmethod for getting all ratings

--- awwwards/models.py
+++ awwwards/models.py
@@ -70,3 +70,14 @@ class Rating(models.Model):
     content_average = models.FloatField(default=0, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
+    
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'
