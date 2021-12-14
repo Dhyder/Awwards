812771168289237fd478bdfe91df0857a5847f95
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-14 09:01:40 +0300

    defined def index

--- awwwards/views.py
+++ awwwards/views.py
@@ -10,3 +10,23 @@ from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, 
 import random
 
 # Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'index.html', {'posts': posts, 'form': form, 'random_post': random_post})
