30de134ddbc7385a2bb3791a30e18bda78fe9b3c
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-12-15 11:14:34 +0300

    del rue

--- rue/lib/python3.8/site-packages/pyuploadcare/dj/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# making South happy
-try:
-    from south.modelsinspector import add_introspection_rules
-
-    add_introspection_rules([], [r"^pyuploadcare\.dj\."])
-except ImportError:
-    pass
